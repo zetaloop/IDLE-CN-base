bca9668d6add1d8e1215ce7df99ab170a210f44a
Terry Jan Reedy <tjreedy@udel.edu>
2016-06-03 22:19:17 -0400

    Issue 20567: Revise idle_test/README.txt and some tests to match new advice.

--- idlelib/idle_test/test_autoexpand.py
+++ idlelib/idle_test/test_autoexpand.py
@@ -1,9 +1,9 @@
-"""Unit tests for idlelib.autoexpand"""
+"""Unit tests for idlelib.AutoExpand"""
 import unittest
 from test.support import requires
 from tkinter import Text, Tk
 #from idlelib.idle_test.mock_tk import Text
-from idlelib.autoexpand import AutoExpand
+from idlelib.AutoExpand import AutoExpand
 
 
 class Dummy_Editwin:
@@ -25,10 +25,10 @@ class AutoExpandTest(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
+        del cls.text, cls.auto_expand
         if hasattr(cls, 'tk'):
             cls.tk.destroy()
             del cls.tk
-        del cls.text, cls.auto_expand
 
     def tearDown(self):
         self.text.delete('1.0', 'end')
